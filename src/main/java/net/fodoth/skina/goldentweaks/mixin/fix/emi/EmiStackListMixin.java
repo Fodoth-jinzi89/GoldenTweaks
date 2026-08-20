b53c4647d7ba2c516f009e494cdf8634e459660e
Fodoth_jinzi89 <wlcypr222@126.com>
2026-08-20 12:20:32 +0800

    fix

--- src/main/java/net/fodoth/skina/goldentweaks/mixin/fix/emi/EmiStackListMixin.java
+++ src/main/java/net/fodoth/skina/goldentweaks/mixin/fix/emi/EmiStackListMixin.java
@@ -1,10 +1,13 @@
 package net.fodoth.skina.goldentweaks.mixin.fix.emi;
 
 import dev.emi.emi.registry.EmiStackList;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.world.item.Item;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.Collections;
 import java.util.concurrent.CopyOnWriteArrayList;
@@ -12,6 +15,21 @@ import java.util.concurrent.CopyOnWriteArrayList;
 @Mixin(value = EmiStackList.class, remap = false)
 public class EmiStackListMixin {
 
+    @Inject(
+            method = "isHiddenFromRecipeViewers",
+            at = @At("HEAD"),
+            cancellable = true
+    )
+    private static <T> void goldentweaks$showTravelopticsItems(T value, CallbackInfoReturnable<Boolean> cir) {
+        if (value instanceof Item item) {
+            var id = BuiltInRegistries.ITEM.getKey(item);
+            if (id.getNamespace().equals("traveloptics")
+                    && !id.getPath().contains("example")
+                    && !id.getPath().contains("dummy")) {
+                cir.setReturnValue(false);
+            }
+        }
+    }
 
     @Inject(
             method = "reload",
